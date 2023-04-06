4504913ac2c098b11022f95be48829dfdc96b0a9
karennovitaaa <karennovita071102@gmail.com>
2023-04-06 13:54:52 +0700

    minggu 6

---
+++ Minggu_6/SQLite/app/src/main/java/com/example/sqlite/DataHelper.java
@@ -0,0 +1,24 @@
+package com.example.sqlite;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class DataHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "biodatadiri.db";
+    private static final int DATABASE_VERSION = 1;
+    public DataHelper(Context context){
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String sql = "create table biodata(no integer primary key, nama text null, tgl text null, jk text null, alamat text null);";
+        Log.d("Data","onCreate: "+ sql);
+        db.execSQL(sql);
+        sql = "INSERT INTO biodata (no, nama, tgl, jk, alamat) VALUES ('1', 'Karen', '2002-11-07', 'Perempuan', 'Solo');";
+        db.execSQL(sql);
+    }
+    @Override
+    public void onUpgrade(SQLiteDatabase arg0, int arg1, int arg2) {
+    }
+}
